a65abb977e914dde421f32389a710440317983ea
Aleaand <alejandraandrade1224@gmail.com>
2026-05-14 11:07:53 +0200

    Corregir nombre

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -96,7 +96,7 @@ const customerController = {
                     u.name as user_name, u.email as user_email, u.phone as user_phone, u.primarylastname as user_lastname,
                     l.*,
                     h.name as hotel_name,
-                    t.name as transfer_name
+                    tf.airline as transfer_name
                 FROM reservations r
                 LEFT JOIN flights f ON r.space_flight_id = f.id
                 LEFT JOIN destinations d ON f.destination_id = d.id
@@ -105,7 +105,7 @@ const customerController = {
                 LEFT JOIN users u ON r.user_id = u.id
                 LEFT JOIN reservation_logistics l ON r.id = l.reservation_id
                 LEFT JOIN hotels h ON l.hotel_id = h.id
-                LEFT JOIN transfers t ON l.transfer_id = t.id
+                LEFT JOIN terrestrial_flights tf ON l.terrestrial_flight_id = tf.id
                 WHERE r.booking_group_id = $1 AND r.user_id = $2
                 LIMIT 1
             `;
